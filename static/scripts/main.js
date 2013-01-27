d1ab2cb0a820d37252676aa871e3c3f8ae173237
Paula Norris <thepandalion@gmail.com>
2013-01-27 17:58:05 +0000

    Show character descriptions on mouseover

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,22 @@
 // Teh Javascripts!
+localStorage.setItem('character_gender','gal');
+var character_gender = localStorage.getItem('character_gender');
+
+$(function() {
+    //Display character descriptions
+    $('.char').on('mouseover', function() {
+        var galDesc = "Totally Awesome Gal";
+        var dudeDesc = "Super Amazing Dude";
+
+        if ($(this).hasClass('gal')) {
+            $('.char-title').html(galDesc);
+        } else {
+            $('.char-title').html(dudeDesc);
+        }
+    });
+
+    $('.char').on('mouseout', function() {
+        var nochoiceDesc = "Choices are tough, but they gotta be made...";
+        $('.char-title').html(nochoiceDesc);
+    });
+});
